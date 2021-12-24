d5859419628e073b96e96bdee6f6e80f94685af3
s749312025 <749312025@qq.com>
2021-12-24 10:27:04 +0800

    first commit

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,50 @@
+const Path = require('path');
+const Webpack = require('webpack');
+const { merge } = require('webpack-merge');
+
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  target: 'web',
+  mode: 'development',
+  devtool: 'eval-cheap-source-map',
+  output: {
+    chunkFilename: 'js/[name].chunk.js',
+  },
+  devServer: {
+    inline: true,
+    hot: true,
+  },
+  plugins: [
+    new Webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.js$/,
+        include: Path.resolve(__dirname, '../src'),
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s?css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+});
